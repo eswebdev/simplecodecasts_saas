0595e62d67d4f9a2cd94b30ef5a9fab1a4eda868
es_webdev <es_webdev@yahoo.com>
2015-11-09 22:33:46 +0000

    updated

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,12 +2,16 @@ class ContactsController < ApplicationController
   def new
     @contact = Contact.new
   end
+  
+  
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
       name = params[:contact][:name]
       email = params[:contact][:email]
       body = params[:contact][:comments]
+      
+      
       ContactMailer.contact_email(name, email, body).deliver
       flash[:success] = 'Message sent.'
       redirect_to new_contact_path
@@ -16,6 +20,7 @@ class ContactsController < ApplicationController
       redirect_to new_contact_path
     end
   end
+  
   private
     def contact_params
       params.require(:contact).permit(:name, :email, :comments)
