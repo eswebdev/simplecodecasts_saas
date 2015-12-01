ba8b8be8c5f220917d6a7486f0b55d20a638d808
es_webdev <es_webdev@yahoo.com>
2015-12-01 23:41:59 +0000

    added css to community

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   
   def index
+    @user = User.all
+    @users = User.includes(:profile)
+
   end
   
   def show
